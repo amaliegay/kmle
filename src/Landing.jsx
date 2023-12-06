f1a9932010817a0c07f3fa8ed33d139e862e199e
Amalie <amaliegay@outlook.com>
2023-12-06 15:40:32 +0800

    split up Icons and Landing

---
+++ src/Landing.jsx
@@ -0,0 +1,15 @@
+import './Landing.css'
+
+function Landing() {
+    return (
+        <section id="landing">
+            <div id="site-title">
+                <h1>KMLE 陈刑很型</h1>
+                <h5>LOVE IS THE ONLY THOUGHT + PAIN IS THE ONLY FEELING</h5>
+                <h5></h5>
+            </div>
+        </section>
+    )
+}
+
+export default Landing
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Icons from './Icons.jsx'
+import Landing from './Landing.jsx'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Icons />
+    <Landing />
   </React.StrictMode>,
 );
