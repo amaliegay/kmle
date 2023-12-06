f64f36d0c68102a84fb7ffd9054a5a24dab1e1e8
Amalie <amaliegay@outlook.com>
2023-12-06 18:58:29 +0800

    vertically aligned h1

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,9 +4,8 @@ function Landing() {
     return (
         <section id="landing">
             <div id="site-title">
-                <h1>KMLE 陈刑很型</h1>
+                <div className='leftToRight'><h1 className='karrik'>KMLE </h1><h1 className='Chinese'>陈刑很型</h1></div>
                 <h5>LOVE IS THE ONLY THOUGHT + PAIN IS THE ONLY FEELING</h5>
-                <h5></h5>
             </div>
         </section>
     )
