eca517d68f9a8f32b044657af1eda866c2d2fd8c
Amalie <amaliegay@outlook.com>
2023-12-06 14:29:49 +0800

    Update App.jsx:陈刑不是邢

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 	return (
 		<section id="landing">
 			<div id="site-title">
-				<h1>KMLE 陈邢很型</h1>
+				<h1>KMLE 陈刑很型</h1>
 				<h5>LOVE IS THE ONLY THOUGHT + PAIN IS THE ONLY FEELING</h5>
 				<h5></h5>
 			</div>
