3442a2f01b7a936fd0f5c68bf72483b07c76cf11
Amalie <amaliegay@outlook.com>
2023-12-07 09:49:26 +0800

    loading spinner

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,13 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import * as Progress from 'react-native-progress';
+
 import './Landing.css'
 
 function Landing() {
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 8000)
+    }, [])
     return (
-        <section id="landing">
-            <div id="site-title">
-                <div className='leftToRight'><h1 className='karrik'>KMLE </h1><h1 className='Chinese'>陈刑很型</h1></div>
-                <h5>LOVE IS THE ONLY THOUGHT + PAIN IS THE ONLY FEELING</h5>
-            </div>
-        </section>
+        <>
+            {
+                loading ?
+                    <Progress.Circle size={30} indeterminate={true} color='rgba(255,255,255,1)' loading={loading} />
+                    :
+                    <section id="landing">
+                        <div id="site-title">
+                            <div className='leftToRight'><h1 className='karrik'>KMLE </h1><h1 className='Chinese'>陈刑很型</h1></div>
+                            <h5>LOVE IS THE ONLY THOUGHT + PAIN IS THE ONLY FEELING</h5>
+                        </div>
+                    </section>
+            }
+        </>
     )
 }
 
