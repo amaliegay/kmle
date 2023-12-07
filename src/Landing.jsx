9a535d7ada36064ca5cb523c9b022aa6376d9477
Amalie <amaliegay@outlook.com>
2023-12-07 14:36:18 +0800

    added back the site tltle

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,15 +4,14 @@ function Landing() {
     return (
         <>
             <div className='loader' />
-        </>
-    )
-}
-
-/* <section id="landing">
+            <section id="landing">
                 <div id="site-title">
                     <div className='leftToRight'><h1 className='karrik'>KMLE </h1><h1 className='Chinese'>陈刑很型</h1></div>
                     <h5>LOVE IS THE ONLY THOUGHT + PAIN IS THE ONLY FEELING</h5>
                 </div>
-            </section>*/
+            </section>
+        </>
+    )
+}
 
 export default Landing
