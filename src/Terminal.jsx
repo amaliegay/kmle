11da5b73e922479dbcf4c40f71cae704fae8e3b5
Amalie <amaliegay@outlook.com>
2023-12-18 16:31:11 +0800

    pink glow

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -3,14 +3,17 @@ import './Terminal.css'
 function Terminal() {
     return (
         <>
-            <div className="consolas glow-green">
-                <pre className='no-margin'> _  _ __  __ _     _____</pre>
-                <pre className='no-margin'>| |/ |  \/  | |   | ____|</pre>
-                <pre className='no-margin'>| ' /| |\/| | |   |  _|</pre>
-                <pre className='no-margin'>| . \| |  | | |___| |___</pre>
-                <pre className='no-margin'>|_|\_|_|  |_|_____|_____|</pre>
+            <div className="terminal">
+                <div className="glow-green">
+                    <pre className='no-margin'> _  _ __  __ _     _____</pre>
+                    <pre className='no-margin'>| |/ |  \/  | |   | ____|</pre>
+                    <pre className='no-margin'>| ' /| |\/| | |   |  _|</pre>
+                    <pre className='no-margin'>| . \| |  | | |___| |___</pre>
+                    <pre className='no-margin'>|_|\_|_|  |_|_____|_____|</pre>
+                </div>
+                <p>For a list of available commands, type `help`.</p>
             </div>
-            <p>For a list of available commands, type `help`.</p>
+
         </>
     )
 }
