2e894695a46085bbba0687a06146ca9341a5ac1e
Amalie <amaliegay@outlook.com>
2023-12-18 17:57:23 +0800

    Terminal commands

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -4,14 +4,28 @@ function Terminal() {
     return (
         <>
             <div className="terminal">
-                <div className="glow-green">
+                <div className="glow-pink">
                     <pre className='no-margin'> _  _ __  __ _     _____</pre>
                     <pre className='no-margin'>| |/ |  \/  | |   | ____|</pre>
                     <pre className='no-margin'>| ' /| |\/| | |   |  _|</pre>
                     <pre className='no-margin'>| . \| |  | | |___| |___</pre>
                     <pre className='no-margin'>|_|\_|_|  |_|_____|_____|</pre>
                 </div>
-                <p>For a list of available commands, type `help`.</p>
+                <p className='terminal-small'>For a list of available commands, type `help`.</p>
+                <div className="terminal-small commands">
+                    <p>Commands:</p>
+                    <table className="paddingBetweenCols">
+                        <tr>
+                            <td><a id="link_gh" href="https://github.com/amaliegay" target="_blank" className="command"><span
+                                className="text-yellow"> github </span></a></td>
+                            <td><a href="https://github.com/amaliegay" target="_blank" class="command"><span class="text-pink">links to
+                                my Github profile</span></a></td>
+                        </tr>
+                    </table>
+                    <br />
+                    <br />
+                    <br />
+                </div>
             </div>
 
         </>
