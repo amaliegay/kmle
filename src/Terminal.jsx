c2f93594ee0aca4b817fb79ec3034233949c801d
Amalie <amaliegay@outlook.com>
2023-12-18 15:06:49 +0800

    Terminal font

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -3,11 +3,11 @@ import './Terminal.css'
 function Terminal() {
     return (
         <>
-            <pre className="mono glow-green">
- _  _ __  __ _     _____
+            <pre className="consolas glow-green">
+ _  _ __  __ _     _____ 
 | |/ |  \/  | |   | ____|
-| ' /| |\/| | |   |  _|
-| . \| |  | | |___| |___
+| ' /| |\/| | |   |  _|  
+| . \| |  | | |___| |___ 
 |_|\_|_|  |_|_____|_____|
             </pre>
         </>
